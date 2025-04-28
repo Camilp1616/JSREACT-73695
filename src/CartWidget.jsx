6a5a7334665d389692e1c1d4192651d7e9a2ebb5
Camila Pizarro <camilapizarro@hotmail.com>
2025-04-28 17:58:14 -0300

    1er Pre-Entrega

---
+++ src/CartWidget.jsx
@@ -0,0 +1,9 @@
+import { Flex } from "@chakra-ui/react";
+import { GiShoppingCart } from "react-icons/gi";
+
+
+const CardWidget = () => {
+return <Flex alignItems='center'><GiShoppingCart size={25} />0</Flex>
+};
+
+export default CardWidget;
